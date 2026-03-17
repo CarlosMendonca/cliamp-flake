8bd83b353f95e04a4673b0de4ee4ae7c7059615b
Carlos Mendonça <CarlosMendonca@users.noreply.github.com>
2026-03-17 03:33:50 -0400

    Initial commit; v1.21.3

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "cliamp - a terminal music player";
+
+  inputs = {
+    # consumers can override: inputs.cliamp.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages.cliamp = pkgs.buildGoModule {
+          pname = "cliamp";
+          version = "1.21.3";
+
+          nativeBuildInputs = [ pkgs.pkg-config ];
+
+          buildInputs = [
+            pkgs.alsa-lib
+            pkgs.libogg
+            pkgs.libvorbis
+            pkgs.flac
+          ];
+
+          src = pkgs.fetchFromGitHub {
+            owner = "bjarneo";
+            repo = "cliamp";
+            rev = "v1.21.3";
+            hash = "sha256-noL8Aea5jFOgahbZ4FTuU81dEofCmAXG3OxrG5ZRStc=";
+          };
+
+          vendorHash = "sha256-UMDCpfSGfvJmI+sImaFzgZpLNaLMgEnmGCqERwPokHM=";
+
+          meta = {
+            description = "A terminal music player built with Bubbletea, Lip Gloss, Beep, and go-librespot";
+            homepage = "https://github.com/bjarneo/cliamp";
+            license = pkgs.lib.licenses.mit;
+            mainProgram = "cliamp";
+          };
+        };
+
+        packages.default = self.packages.${system}.cliamp;
+
+        apps.cliamp = flake-utils.lib.mkApp {
+          drv = self.packages.${system}.cliamp;
+        };
+
+        apps.default = self.apps.${system}.cliamp;
+      });
+}
