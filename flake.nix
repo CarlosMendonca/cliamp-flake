14d43edca8fbf55acd7ee02ecb691503ea336ebb
Carlos Mendonça <CarlosMendonca@users.noreply.github.com>
2026-04-06 10:53:44 -0400

    fix: changing to nixpgks-unstable to get Go 1.26, which is now required by cliamp

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "cliamp - a terminal music player";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
